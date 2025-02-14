adc08ba4bac4eba0a1d592914908fa0780c23d8a
AlexandrD\AlexandrD <sans357@gmail.com>
2025-02-14 15:47:58 +0200

    hw-07

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
 
@@ -8,12 +7,4 @@ export const store = configureStore({
     contacts: contactsReducer,
     filter: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
